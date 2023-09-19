dc13e5c97d2d5f2fadf9b9f31b8ac606d9449953
SushyDev <xsushy@hotmail.com>
2023-09-19 23:46:58 +0200

    dingus

--- api/user/logout.ts
+++ api/user/logout.ts
@@ -3,7 +3,10 @@ import ErrorHandler from '../../utils/error-handling.js';
 
 export default function handler(_request: VercelRequest, response: VercelResponse) {
     try {
-        response.setHeader('Set-Cookie', `refresh_token=; path=/; HttpOnly; Secure; SameSite=Strict; Max-Age=0;`)
+        response.setHeader('Set-Cookie', [
+            `refresh_token=; path=/; HttpOnly; Secure; SameSite=Strict; Max-Age=0;`,
+            `user=; path=/; HttpOnly; Secure; SameSite=Strict; Max-Age=0;`
+        ])
 
         response.redirect('http://localhost:3000/');
     } catch (error) {
