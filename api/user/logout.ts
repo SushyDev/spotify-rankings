62e3ad8dc62660efca0701c568edcbdc65ca2b97
SushyDev <xsushy@hotmail.com>
2023-09-19 23:42:42 +0200

    dingus

--- api/user/logout.ts
+++ api/user/logout.ts
@@ -1,12 +1,9 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import ErrorHandler from '../../utils/error-handling.js';
 
-export default function handler(request: VercelRequest, response: VercelResponse) {
+export default function handler(_request: VercelRequest, response: VercelResponse) {
     try {
-        response.setHeader('Set-Cookie', [
-            `access_token=; path=/; HttpOnly; Secure; SameSite=Strict; Max-Age=0;`,
-            `refresh_token=; path=/; HttpOnly; Secure; SameSite=Strict; Max-Age=0;`
-        ])
+        response.setHeader('Set-Cookie', `refresh_token=; path=/; HttpOnly; Secure; SameSite=Strict; Max-Age=0;`)
 
         response.redirect('http://localhost:3000/');
     } catch (error) {
